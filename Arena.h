cbad45bb2a8d432271f09f5710a400ca00a98245
Hap1ma <1132210518@pfur.ru>
2023-06-20 19:26:10 +0300

    Обновил некоторые файлы

--- Arena.h
+++ Arena.h
@@ -2,34 +2,34 @@
 #define ARENA_H
 
 #include <vector>
-#include "Prey.h"    // Включаем заголовочный файл с определением класса Prey
-#include "Predator.h" // Включаем заголовочный файл с определением класса Predator
+#include "Predator.h"
+#include "Prey.h"
 
 // Класс для представления арены
 class Arena {
 private:
-    int width;               // Ширина арены
-    int height;              // Высота арены
-    std::vector<Prey> prey;   // Вектор жертв
-    std::vector<Predator> predator; // Вектор хищников
+    int width;                          // Ширина арены
+    int height;                         // Высота арены
+    std::vector<Predator> predators;    // Вектор хищников
+    std::vector<Prey> prey;              // Вектор жертв
 
 public:
     // Конструктор с параметрами
     Arena(int width, int height);
 
-    // Метод для добавления жертвы на арену
-    void addPrey(const Prey& newPrey);
-
     // Метод для добавления хищника на арену
-    void addPredator(const Predator& newPredator);
+    void addPredator(const Predator& predator);
 
-    // Метод для выполнения одного шага симуляции
-    void simulateStep();
+    // Метод для добавления жертвы на арену
+    void addPrey(const Prey& prey);
 
-    // Метод, возвращающий ширину арены
+    // Метод для запуска процесса охоты
+    void startHunting();
+
+    // Метод для получения ширины арены
     int getWidth() const;
 
-    // Метод, возвращающий высоту арены
+    // Метод для получения высоты арены
     int getHeight() const;
 };
 
